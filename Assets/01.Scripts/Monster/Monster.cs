5353100ef4a9267edaffd33942ea78b3623aedb2
omyman12 <qkrrlcks00@gmail.com>
2024-12-04 10:28:15 +0900

    [Complex] - TestScene duplicate, cs추가
    [chore] - TestScene 맵 개인씬으로 복제
    [chore] - Monster 관련 스크립트 생성

---
+++ Assets/01.Scripts/Monster/Monster.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class  Monster : MonoBehaviour
+{
+    [SerializeField] protected Monster MonsterController;
+    [SerializeField] protected Monster MonsterCondition;
+    // [SerializeField] protected Monster
+
+}
