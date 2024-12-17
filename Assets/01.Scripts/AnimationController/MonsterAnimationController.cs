6495cf0f5384e698481474d8a1d41ca073fb11fc
sh010207 <sh010207@naver.com>
2024-12-17 20:05:08 +0900

    [Update] - 공격 모션 중 다른 곳으로 모션나가지 못하게 업데이트

--- Assets/01.Scripts/AnimationController/MonsterAnimationController.cs
+++ Assets/01.Scripts/AnimationController/MonsterAnimationController.cs
@@ -5,10 +5,37 @@ public class MonsterAnimationController : AnimationController
 {
     private int isRun = Animator.StringToHash("isRun");
     private int Attack = Animator.StringToHash("Attack");
+    private int isDie = Animator.StringToHash("isDie");
+    private int isHit = Animator.StringToHash("isHit");
+
+    private SpriteRenderer effectSprite;
 
     protected override void Awake()
     {
         base.Awake();
+    }
+
+    private void LateUpdate()
+    {
+        //if(Sprite.flipX)
+        //{
+        //    effectSprite.flipX = true;
+        //    transform.GetChild(0).gameObject.transform.localPosition = new UnityEngine.Vector2(0.5f, 0.2f);
+        //}
+        //if(!Sprite.flipX)
+        //{
+        //    effectSprite.flipX = false;
+        //    transform.GetChild(0).gameObject.transform.localPosition = new UnityEngine.Vector2(-0.5f, 0.2f);
+        //}
+    }
+
+    protected override void EnsureComponents()
+    {
+        base.EnsureComponents();
+        if(effectSprite == null)
+        {
+            effectSprite = GetComponentInChildren<SpriteRenderer>(true);
+        }
     }
 
     public void OnIdle()
@@ -28,8 +55,11 @@ public class MonsterAnimationController : AnimationController
 
     public void OnHit()
     {
-        Animator.SetTrigger("isHit");
+        Animator.SetTrigger(isHit);
     }
 
-
+    public void OnDie()
+    {
+        Animator.SetTrigger(isDie);
+    }
 }
