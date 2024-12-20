4fe251f0bf1a022b03dd6967446f70c4c8eddfeb
omyman12 <qkrrlcks00@gmail.com>
2024-12-20 16:41:52 +0900

    [Complex] - PromptUI 프리팹 제작, InventroyUI.cs 수정
    [Chore] - PomptUI 프리팹 제작
    [Update] - InventoryUI.cs 수정

--- Assets/01.Scripts/UI/InventoryUI.cs
+++ Assets/01.Scripts/UI/InventoryUI.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
+
 
 public class InventoryUI : UIBase
 {
@@ -15,90 +14,39 @@ public class InventoryUI : UIBase
     [SerializeField] private TextMeshProUGUI attackSpeedText;
     [SerializeField] private TextMeshProUGUI moveSpeedText;
     
-    [SerializeField] private TextMeshProUGUI itemNameText1;
-    [SerializeField] private TextMeshProUGUI itemDescription1;
-    [SerializeField] private TextMeshProUGUI itemAttackPower1;
-    [SerializeField] private TextMeshProUGUI itmeType1;
-    [SerializeField] private TextMeshProUGUI itemGrade1;
-    [SerializeField] private TextMeshProUGUI itemEatValue1;
-    private Sprite itmeSprite1;
-    
-    private PlayerStatManager playerStatManager;
-     private PlayerEquipment playerEquipment;
+    [SerializeField] private List<TextMeshProUGUI> itemNameText;
+    [SerializeField] private List<TextMeshProUGUI> itemDescription;
+    [SerializeField] private List<TextMeshProUGUI> itemAttackPower;
+    [SerializeField] private List<TextMeshProUGUI> itmeType;
+    [SerializeField] private List<TextMeshProUGUI> itemGrade;
+    [SerializeField] private List<TextMeshProUGUI> itemEatValue;
+    private Sprite itemSprite1;
 
-    private void Awake()
-    {
-        playerStatManager = GameManager.Instance.player.GetComponent<PlayerStatManager>();
-        playerEquipment = GameManager.Instance.player.GetComponent<PlayerEquipment>();
-    }
-    private void OnEnable()
-    {
-        playerStatManager.OnSubscribeToStatUpdateEvent += UpdateUI;
-        //maxHealthText.text= playerStatManager.stat.GetStatValue("MaxHealth").ToString();
-        //playerEquipment.OnEquipWeaponChanged += OnEquipWeaponChanged;
-    }
-    private void OnDisable()
-    {
-        playerStatManager.OnSubscribeToStatUpdateEvent -= UpdateUI;
-        //playerEquipment.OnEquipWeaponChanged -= OnEquipWeaponChanged;
-    }
-     private void Start()
-     {
-         
-     }
+    private SaveDataContainer saveDataContainer;
 
-    /*private void OnEquipWeaponChanged(WeaponSO weaponData)
-    {
-        itemNameText1.text = $"{weaponData.weaponName}";
-        Debug.Log(weaponData.weaponName);
-        itemDescription1.text = $"{weaponData.description}";
-        itemAttackPower1.text = $"공격력 : {weaponData.attackPower}";
-        itmeType1.text = $"타입 : {weaponData.type}";
-        itemGrade1.text = $"등급 : {weaponData.gradeType}";
-        itemEatValue1.text = $"섭취 증가량 : {weaponData.eatValue}";
-        itmeSprite1 = weaponData.weaponSprite;
-    }*/
 
-    private void UpdateUI(string statKey, float value)
+    private void Update()
     {
-               
-        switch (statKey)
-        {
-            case "MaxHealth":
-                maxHealthText.text = $"{"최대체력",-15}: {value,15}";
-                Debug.Log(maxHealthText);
-                break;
-            case "CurrentHealth":
-                currentHealthText.text = $"/ {value}";
-                break;
-            case "CurrentAttackPower":
-                attackPowerText.text = $"{"공격력",-17}: {value,15}";
-                break;
-            case "CurrentDefense":
-                defenseText.text = $"{"방어력",-17}: {value,15}";
-                break;
-            case "AttackSpeed":
-                attackSpeedText.text = $"{"공격속도",-15}: {value,15}";
-                break;
-            case "MoveSpeed":
-                moveSpeedText.text = $"{"이동속도",-15}: {value,15}";
-                break;
-        }
+        if (saveDataContainer != null) GetDataToText();
     }
 
     public override void Open()
-    {
+    { 
+        if (saveDataContainer == null) GetStatData();
         base.Open();
-
-        /*if ()
-        {
-            itemDescription1.text = "무기 SO에 있는 설명";
-        }
-        else
-        {
-            itemDescription2.text = "무기 SO에 있는 설명..";
-        }
-*/
+    }
+    private void GetStatData()
+    {
+        saveDataContainer = PlayerInfoManager.Instance.GetSaveData();
     }
 
+    private void GetDataToText()
+    {
+        maxHealthText.text = ($"최대체력 : {saveDataContainer.playerStatData.maxHealth.ToString()}");
+        attackPowerText.text = ($" 공격력 : {saveDataContainer.playerStatData.currentAttackPower.ToString()}");
+        defenseText.text = ($" 방어력 : {saveDataContainer.playerStatData.currentDefense.ToString()}");
+        attackSpeedText.text = ($" 공격속도 : {saveDataContainer.playerStatData.attackSpeed.ToString()}");
+        moveSpeedText.text = ($" 이동속도 : {saveDataContainer.playerStatData.moveSpeed.ToString()}");
+        //maxHealthText.text = saveDataContainer.playerStatData.maxHealth.ToString();
+    }
 }
--- Assets/01.Scripts/UI/PlayerCanvasUI.cs
+++ Assets/01.Scripts/UI/PlayerCanvasUI.cs
@@ -1,8 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCanvasUI : UIBase
 {
+    [SerializeField] private TextMeshProUGUI currentHealthText;
+    [SerializeField] private TextMeshProUGUI inGameMoneyText;
+    [SerializeField] private TextMeshProUGUI lobbyMoneyText;
+    [SerializeField] private Image healthBar;
+    private SaveDataContainer saveDataContainer;
 
+    private void Update()
+    {
+        if (saveDataContainer != null)
+        {
+            GetDataToText();
+            healthBar.fillAmount = saveDataContainer.playerStatData.currentHealth;
+        }
+    }
+
+    public override void Open()
+    {
+        if (saveDataContainer == null)
+        {
+            GetStatData();
+            healthBar = GetComponent<Image>();
+        }
+        base.Open();
+    }
+    private void GetStatData()
+    {
+        saveDataContainer = PlayerInfoManager.Instance.GetSaveData();
+    }
+
+    private void GetDataToText()
+    {
+        currentHealthText.text = ($"{saveDataContainer.playerStatData.maxHealth.ToString()}/{saveDataContainer.playerStatData.currentHealth.ToString()}");
+        //inGameMoneyText.text = 
+        //lobbyMoneyText.text =
+    }
 }
