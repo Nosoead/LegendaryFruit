85a0b6de0fd6a6a7454f3e76496a9008c601c4f9
Nosoead <thseodh@gmail.com>
2024-12-24 20:48:07 +0900

    [Feat] - ItemManager 추가

---
+++ Assets/01.Scripts/Managers/ItemManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    private List<WeaponSO> itemList = new List<WeaponSO>();
+    private Dictionary<int, WeaponSO> itemDictionary = new Dictionary<int, WeaponSO>();
+    private string path;
+
+    private void Start()
+    {
+        path = "Item";
+        SetItemDictionary();
+    }
+
+    private void SetItemDictionary()
+    {
+        WeaponSO[] itemArray = ResourceManager.Instance.LoadAllResources<WeaponSO>($"{path}");
+        itemList = itemArray.ToList();
+        foreach (WeaponSO item in itemList)
+        {
+            if (!itemDictionary.ContainsKey(item.ID))
+            {
+                itemDictionary.Add(item.ID, item);
+            }
+        }
+    }
+
+    public WeaponSO[] GetItemData(int selectNum)
+    {
+        WeaponSO[] selectedItem = new WeaponSO[selectNum];
+        int[] selectedNumber = RandomNumber(selectNum, itemList.Count);
+        for (int i = 0; i < selectedNumber.Length; i++)
+        {
+            selectedItem[i] = itemList[i];
+        }
+        return selectedItem;
+    }
+
+    private int[] RandomNumber(int selectNum, int maxNum)
+    {
+        int[] resultNum = new int[selectNum];
+        int[] numberArray = new int[maxNum];
+        for (int i = 0; i < numberArray.Length; i++)
+        {
+            numberArray[i] = i;
+        }
+
+        for (int i = 0; i < selectNum; i++)
+        {
+            int randomNum = Random.Range(0, maxNum - i);
+            resultNum[i] = numberArray[randomNum];
+            numberArray[randomNum] = numberArray[numberArray.Length - i];
+        }
+        return resultNum;
+    }
+    //TODO 랜덤으로 뽑힌 숫자를 ID값에 맞도록 바꾸는 함수 필요.
+}
--- Assets/01.Scripts/ScriptableObject/WeaponSO.cs
+++ Assets/01.Scripts/ScriptableObject/WeaponSO.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "WeaponSO", menuName = "ScriptableObject/WeaponSO", order = 2)]
 public class WeaponSO : GameSO
 {
+    //   A0B0CD -> A : 아이템등급 / B : 아이템타입 / C : 아이템인덱스(0~999)
+    //ex)13012 -> 무기보상 3-1스테이지 2버전 / 현재는 그냥 1스테이지에 전부 1버전으로
+    public int ID;
     public string weaponName;
     public string description;
     public float attackPower;
