81b6c42fe55934d05755f80ae79e841b3cd36e07
Nosoead <thseodh@gmail.com>
2025-01-03 10:33:41 +0900

    [Complex] - 에셋 추가 및 Stage 재구성

--- Assets/01.Scripts/Stages/Parallax.cs
+++ Assets/01.Scripts/Stages/Parallax.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Cinemachine;
+using System.Runtime.CompilerServices;
 
 public class Parallax : MonoBehaviour
 {
@@ -7,29 +8,46 @@ public class Parallax : MonoBehaviour
     public GameObject cam;
     public float parallaxEffect;
 
+    private void Awake()
+    {
+        Init();
+    }
+
     private void Start()
+    {
+        SetPositionAndLength();
+    }
+
+    private void FixedUpdate()
+    {
+        SetBackground();
+    }
+
+    public void Init()
+    {
+        //cam = 
+    }
+
+    private void SetPositionAndLength()
     {
         startpos = transform.position.x;
         length = (GetComponent<SpriteRenderer>().bounds.size.x);
     }
 
-    private void FixedUpdate()
+    private void SetBackground()
     {
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float distance = (cam.transform.position.x * parallaxEffect);
 
         transform.position = new Vector3(startpos + distance, transform.position.y, transform.position.z);
 
-        if (temp > startpos + length/2)
+        if (temp > startpos + length / 2)
         {
             startpos += length;
-            Debug.Log(startpos);
         }
-        else if (temp < startpos - length/2)
+        else if (temp < startpos - length / 2)
         {
             startpos -= length;
-            Debug.Log(startpos);
         }
-
     }
 }
