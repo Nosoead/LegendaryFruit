025445984281e12830a41a060f665287d1e7deab
omyman12 <qkrrlcks00@gmail.com>
2025-01-07 14:32:03 +0900

    [Complex] - UI프리팹 수정,PromptUI.cs
    [Chore] - PromptUI프리팹 수정
    [Feat] - PromptUI.cs

---
+++ Assets/01.Scripts/UI/PromptUI.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PromptUI : UIBase
+{
+   private TextMeshProUGUI gradeText;
+   private TextMeshProUGUI promptName; 
+   private TextMeshProUGUI promptDescription;
+   private Image promptImage;
+
+   public override void Open()
+   {
+      base.Open();
+   }
+
+   private void DataToPromptText()
+   {
+      /*gradeText.text = ;
+      promptName.text = ;
+      promptDescription.text = ;
+      promptImage = ;*/
+   }
+   
+   // IInteractable로 무기정보 가져오기?
+   
+   //F 꾹누를때 fillamount 조정
+}
