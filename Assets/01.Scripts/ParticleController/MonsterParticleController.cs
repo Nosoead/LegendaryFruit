bdebb19e2dd9ebe1622fcad3715bc61194078d30
sh010207 <sh010207@naver.com>
2025-01-20 12:45:11 +0900

    [Chore] - CurrencySeed 파티클 추가

--- Assets/01.Scripts/ParticleController/MonsterParticleController.cs
+++ Assets/01.Scripts/ParticleController/MonsterParticleController.cs
@@ -5,39 +5,48 @@ using UnityEngine;
 public class MonsterParticleController : ParticleController
 {
     [SerializeField] private MonsterStatManager monsterStatManager;
-    [SerializeField] private ParticleSystem dieParticle;
+    [SerializeField] private ParticleSystem dieParticlePeel;
+    [SerializeField] private ParticleSystem dieParticleSeed;
 
     private void Awake()
     {
-        
+
         EnsureComponents();
     }
 
     private void OnEnable()
     {
         monsterStatManager.DamageTakenEvent += OnDamageReceived;
-        monsterStatManager.OnDieEvent += DieParticlePlay;
+        monsterStatManager.OnDieEvent += DieParticlePeelPlay;
+        monsterStatManager.OnDieEvent += DieParticleSeedPlay;
     }
     private void OnDisable()
     {
         monsterStatManager.DamageTakenEvent -= OnDamageReceived;
-        monsterStatManager.OnDieEvent -= DieParticlePlay;
+        monsterStatManager.OnDieEvent -= DieParticlePeelPlay;
+        monsterStatManager.OnDieEvent -= DieParticleSeedPlay;
     }
 
     private void EnsureComponents()
     {
-        if(monsterStatManager == null)
+        if (monsterStatManager == null)
         {
-            monsterStatManager = GetComponent<MonsterStatManager>();    
+            monsterStatManager = GetComponent<MonsterStatManager>();
         }
-        if(particle == null)
+        if (particle == null)
         {
             particle = GetComponentInChildren<ParticleSystem>();
         }
     }
 
-    private void DieParticlePlay()
-    {     
-        dieParticle.Play();
+    private void DieParticlePeelPlay()
+    {
+        dieParticlePeel.Play();
+    }
+
+    private void DieParticleSeedPlay()
+    {
+        dieParticleSeed.gameObject.SetActive(true);
+        dieParticleSeed.Play();
     }
 }
